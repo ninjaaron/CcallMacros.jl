fe1c77a08baa3d90a95da8ce5df200be62a9440e
Aaron Christianson <ninjaaron@gmail.com>
2019-07-28 17:05:34 +0200

    added support for varargs

--- src/CcallMacros.jl
+++ src/CcallMacros.jl
@@ -1,15 +1,20 @@
 module CcallMacros
 export @ccall, @cdef, @disable_sigint, @check_syserr
 
+struct NoType end
+
+getfunc(f) = QuoteNode(f)
+getfunc(f::Expr) = :(($(f.args[2]), $(f.args[1])))
 
 """
-`parsecall` is an implementation detail of @ccall and @cdef
+`parsecall` is an implementation detail of `@ccall` and `@cdef`
 
-takes and expression like :(printf("%d"::Cstring, value::Cuint)::Cvoid)
-returns: a tuple of (function_name, return_type, arg_types, args
+takes and expression like `:(printf("%d"::Cstring, value::Cuint)::Cvoid)`
+returns: a tuple of `(function_name, return_type, arg_types, args)`
 
 The above input outputs this:
-(:printf, :Cvoid, :((Cstring, Cuint)), ["%d", :value])
+
+    (:printf, :Cvoid, :((Cstring, Cuint)), ["%d", :value])
 
 Note that the args are in an array, not a quote block and have to be
 appended to the ccall in a separate step.
@@ -25,31 +30,57 @@ function parsecall(expr)
         error("@ccall has to be a function call")
 
     # get the function symbols
-    getfunc(f) = QuoteNode(f)
-    getfunc(f::Expr) = :(($(f.args[2]), $(f.args[1])))
     func = getfunc(call.args[1])
 
+    hasvarargs = false
+    firstarg = call.args[2]
+    if firstarg isa Expr && firstarg.head === :parameters
+        length(firstarg.args) > 1 || (kw = firstarg.args[1].args)[1] !== :varargs &&
+            error("@ccall only takes one keyword argument: varargs")
+        hasvarargs = true
+        vararg_type = kw[2]
+        allargs = call.args[3:end]
+    else
+        allargs = call.args[2:end]
+    end
+
     # separate annotations from names
-    mkarg(arg) = arg.head != :(::) ?
-        error("args in @ccall must be annotated") :
-        (arg=arg.args[1], type_=arg.args[2])
+    in_vararg::Bool = false
+    function mkarg(arg)
+        in_vararg && return (arg=arg, type=NoType)
+        if hasvarargs && !isa(arg, Expr)
+            in_vararg = true
+            return mkarg(arg)
+        end
 
-    pairs = mkarg.(call.args[2:end])
+        if arg.head != :(::)
+            error("args in @ccall must be annotated")
+        end
+        type = if in_vararg
+            :($(arg.args[2])...)
+        else
+            arg.args[2]
+        end
+        return (arg=arg.args[1], type=type)
+    end
+
+    pairs = mkarg.(allargs)
     args = [a.arg for a in pairs]
+    types = Any[a.type for a in pairs if a.type !== NoType]
+    hasvarargs && push!(types, :($vararg_type...))
     argtypes = :(())
-    argtypes.args = [a.type_ for a in pairs]
-
+    argtypes.args = types
     return func, rettype, argtypes, args
 end
 
 """
 convert a julia-style function definition to a ccall:
 
-`@ccall printf("%d"::Cstring, 10::Cint)::Cvoid`
+    @ccall printf("%d"::Cstring, 10::Cint)::Cvoid
 
 same as:
 
-`ccall(:printf, Cvoid, (Cstring, Cint), "%d", 10)`
+    ccall(:printf, Cvoid, (Cstring, Cint), "%d", 10)
 """
 macro ccall(expr)
     func, rettype, argtypes, args = parsecall(expr)
@@ -62,11 +93,11 @@ end
 define a _very_ thin wrapper function on a ccall. Mostly for wrapping
 libraries quickly as a foundation for a higher-level interface.
 
-@cdef mkfifo(path::Cstring, mode::Cuint)::Cint
+    @cdef mkfifo(path::Cstring, mode::Cuint)::Cint
 
 becomes:
 
-mkfifo(path, mode) = ccall(:mkfifo, Cint, (Cstring, Cuint), path, mode)
+   mkfifo(path, mode) = ccall(:mkfifo, Cint, (Cstring, Cuint), path, mode)
 """
 macro cdef(expr)
     func, rettype, argtypes, args = parsecall(expr)
