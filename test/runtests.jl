45d81889819afd2f8d29638f553bb1a2638752cd
Aaron Christianson <ninjaaron@gmail.com>
2019-07-29 23:52:54 +0200

    added more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,6 @@
 using CcallMacros: @ccall, @cdef, @disable_sigint, @check_syserr, parsecall
 using Test
 
-const STRING = "hello"
-const BUFFER = Ptr{UInt8}(Libc.malloc((length(STRING) + 1) * sizeof(Cchar)))
 const CALLEXPR = :(
     libc.printf("%d"::Cstring, value::Cuint)::CVoid
 )
@@ -16,6 +14,31 @@ const CALLEXPR = :(
 )
 
 # test ccall
+call = @macroexpand @ccall libstring.func(
+    str::Cstring,
+    num1::Cint,
+    num2::Cint
+)::Cstring
+
+@test call == :(
+    ccall((:func, libstring), Cstring,
+          (Cstring, Cint, Cint), str, num1, num2)
+)
+
+# test ccall with varargs
+call = @macroexpand @ccall printf(
+    "%d, %d, %d\n"::Cstring, 1, 2, 3; varargs=Cint
+)::Cint
+@test call == :(
+    ccall(:printf, Cint, (Cstring, Cint...), "%d, %d, %d\n", 1, 2, 3)
+)
+
+
+# call some c functions
+@test @ccall(sqrt(4.0::Cdouble)::Cdouble) == 2.0
+
+const STRING = "hello"
+const BUFFER = Ptr{UInt8}(Libc.malloc((length(STRING) + 1) * sizeof(Cchar)))
 @ccall strcpy(BUFFER::Cstring, STRING::Cstring)::Cstring
 @test unsafe_string(BUFFER) == STRING
 
