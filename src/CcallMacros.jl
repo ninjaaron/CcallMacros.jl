d7cd5d2d6c42dcb2aed9583720f3dfd3955e7743
Aaron Christianson <ninjaaron@gmail.com>
2019-08-03 19:41:38 +0200

    brought up to parity with my ccallmacro feature for julialang/julia

--- src/CcallMacros.jl
+++ src/CcallMacros.jl
@@ -2,14 +2,59 @@ module CcallMacros
 export @ccall, @cdef, @disable_sigint, @check_syserr
 
 struct NoType end
+struct CcallError{T <: AbstractString} <: Exception
+    msg::T
+end
+Base.showerror(io::IO, e::CcallError) = print(io, "CcallError: ", e.msg)
 
-getfunc(f) = QuoteNode(f)
-getfunc(f::Expr) = :(($(f.args[2]), $(f.args[1])))
+getcfunc(f) = QuoteNode(f)
+getcfunc(f::Expr) = :(($(f.args[2]), $(f.args[1])))
+hashead(symbol, _) = false
+hashead(expr::Expr, head) = expr.head === head
 
 """
-`parsecall` is an implementation detail of `@ccall` and `@cdef`
+Determine if there are varargs and return a vector of all arguments
+based on the call signature.
 
-takes and expression like `:(printf("%d"::Cstring, value::Cuint)::Cvoid)`
+returns a tuple of `(hasvarargs, arguments)`
+"""
+function getargs(call)
+    firstarg = call.args[2]
+    if hashead(firstarg, :parameters)
+        return call.args[3:end], firstarg.args
+    else
+        return call.args[2:end], []
+    end
+end
+
+"""
+       mkarg(arg)
+
+`mkarg` takes an argument and returns a tuple of symbol and type. The
+argument must have a type annotation or an error will be thrown.
+"""
+function mkarg(arg)
+    !hashead(arg, :(::)) &&
+        throw(CcallError("args in @ccall need type annotations. '$arg' doesn't have one."))
+    return (arg=arg.args[1], type=arg.args[2])
+end
+
+"""
+get the type specified on the varargs. throw if there is more than one type.
+"""
+function getvarargtype(varargs)
+    vararg_types = Set(a.type for a in varargs)
+    length(vararg_types) > 1 &&
+        throw(CcallError("varargs @ccall with different argument types not yet supported"))
+    return pop!(vararg_types)
+end
+
+"""
+    parsecall(expression)
+
+`parsecall` is an implementation detail of `@ccall
+
+it takes and expression like `:(printf("%d"::Cstring, value::Cuint)::Cvoid)`
 returns: a tuple of `(function_name, return_type, arg_types, args)`
 
 The above input outputs this:
@@ -21,66 +66,66 @@ appended to the ccall in a separate step.
 """
 function parsecall(expr)
     # setup and check for errors
-    expr.head != :(::) &&
-        error("@ccall needs a function signature with a return type")
+    !hashead(expr, :(::)) &&
+        throw(CcallError("@ccall needs a function signature with a return type"))
     rettype = expr.args[2]
 
     call = expr.args[1]
-    call.head != :call &&
-        error("@ccall has to be a function call")
+    !hashead(call, :call) &&
+        throw(CcallError("@ccall has to take a function call"))
 
     # get the function symbols
-    func = getfunc(call.args[1])
-
-    hasvarargs = false
-    firstarg = call.args[2]
-    if firstarg isa Expr && firstarg.head === :parameters
-        length(firstarg.args) > 1 || (kw = firstarg.args[1].args)[1] !== :varargs &&
-            error("@ccall only takes one keyword argument: varargs")
-        hasvarargs = true
-        vararg_type = kw[2]
-        allargs = call.args[3:end]
-    else
-        allargs = call.args[2:end]
-    end
+    func = getcfunc(call.args[1])
+    normalargs, varargs = getargs(call)
 
     # separate annotations from names
-    in_vararg::Bool = false
-    function mkarg(arg)
-        in_vararg && return (arg=arg, type=NoType)
-        if hasvarargs && !isa(arg, Expr)
-            in_vararg = true
-            return mkarg(arg)
-        end
-
-        if arg.head != :(::)
-            error("args in @ccall must be annotated")
-        end
-        type = if in_vararg
-            :($(arg.args[2])...)
-        else
-            arg.args[2]
-        end
-        return (arg=arg.args[1], type=type)
-    end
-
-    pairs = mkarg.(allargs)
-    args = [a.arg for a in pairs]
-    types = Any[a.type for a in pairs if a.type !== NoType]
-    hasvarargs && push!(types, :($vararg_type...))
+    normalargs = mkarg.(normalargs)
+    args = Any[a.arg for a in normalargs]
+    types = Any[a.type for a in normalargs]
     argtypes = :(())
     argtypes.args = types
+
+    isempty(varargs) && return func, rettype, argtypes, args
+
+    # vararg handling. to be changed if rebased on foreigncall.
+    varargs = mkarg.(varargs)
+    append!(args, (a.arg for a in varargs))
+    vararg_type = getvarargtype(varargs)
+    push!(types, :($vararg_type...))
     return func, rettype, argtypes, args
 end
 
 """
+    @ccall(call expression)
+
 convert a julia-style function definition to a ccall:
 
-    @ccall printf("%d"::Cstring, 10::Cint)::Cvoid
+    @ccall printf("%d"::Cstring, 10::Cint)::Cint
 
 same as:
 
-    ccall(:printf, Cvoid, (Cstring, Cint), "%d", 10)
+    ccall(:printf, Cint, (Cstring, Cint), "%d", 10)
+
+All arguments must have type annotations and the return type must also
+be annotated.
+
+varargs are supported with the following convention:
+
+    @ccall printf("%d, %d, %d"::Cstring ; 1::Cint, 2::Cint, 3::Cint)::Cint
+
+Mind the semicolon. Note that, as with the current ccall API, all
+varargs must be of the same type.
+
+Using functions from other libraries is supported by prefixing
+the function name with the name of the C library, like this:
+
+    const glib = "libglib-2.0"
+    @ccall glib.g_uri_escape_string(
+        uri::Cstring, ":/"::Cstring, true::Cint
+    )::Cstring
+
+The string literal could also be used directly before the symbol of
+the function name, if desired `"libglib-2.0".g_uri_escape_string(...`
 """
 macro ccall(expr)
     func, rettype, argtypes, args = parsecall(expr)
@@ -123,21 +168,28 @@ macro disable_sigint(expr)
     esc(out)
 end
 
-const comment = r"#=.*?=# "
+"""
+    nolinenum(expr)
+
+remove `LineNumberNodes`
+"""
+nolinenum(s) = s
+nolinenum(e::Expr) =
+    Expr(e.head, (nolinenum(a) for a in e.args if !isa(a, LineNumberNode))...)
 
 """
 throw a system error if the expression returns a non-zero exit status.
 """
 macro check_syserr(expr, message=nothing)
     if message == nothing
-        message = replace(string(expr), comment => "")
+        message = nolinenum(expr) |> string
     end
-    out = quote
-        err = $expr
-        systemerror($str, err != 0)
+    return quote
+        err = $(esc(expr))
+        systemerror($message, err != 0)
         err
     end
-    esc(out)
 end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,55 +1,72 @@
-using CcallMacros: @ccall, @cdef, @disable_sigint, @check_syserr, parsecall
 using Test
 
-const CALLEXPR = :(
-    libc.printf("%d"::Cstring, value::Cuint)::CVoid
-)
+using CcallMacros: @ccall, parsecall, CcallError
 
-# test parsecall
-@test parsecall(CALLEXPR) == (
-    :(:printf, libc),     # function
-    :CVoid,               # return type
-    :((Cstring, Cuint)),  # argument types
-    ["%d", :value]        # argument symbols
-)
+@testset "test basic parsecall functionality" begin
+    callexpr = :(
+        libc.printf("%d"::Cstring, value::Cuint)::Cvoid
+    )
+    @test parsecall(callexpr) == (
+        :(:printf, libc),     # function
+        :Cvoid,               # return type
+        :((Cstring, Cuint)),  # argument types
+        ["%d", :value]        # argument symbols
+    )
+end
 
-# test ccall
-call = @macroexpand @ccall libstring.func(
-    str::Cstring,
-    num1::Cint,
-    num2::Cint
-)::Cstring
+@testset "ensure the base-case of @ccall works, including library name" begin
+    call = @macroexpand @ccall libstring.func(
+        str::Cstring,
+        num1::Cint,
+        num2::Cint
+    )::Cstring
 
-@test call == :(
-    ccall((:func, libstring), Cstring,
-          (Cstring, Cint, Cint), str, num1, num2)
-)
+    @test call == :(
+        ccall((:func, libstring), Cstring,
+            (Cstring, Cint, Cint), str, num1, num2)
+    )
+end
 
-# test ccall with varargs
-call = @macroexpand @ccall printf(
-    "%d, %d, %d\n"::Cstring, 1, 2, 3; varargs=Cint
-)::Cint
-@test call == :(
-    ccall(:printf, Cint, (Cstring, Cint...), "%d, %d, %d\n", 1, 2, 3)
-)
+@testset "ensure @ccall handles varargs correctly" begin
+    call = @macroexpand @ccall printf(
+        "%d, %d, %d\n"::Cstring ; 1::Cint, 2::Cint, 3::Cint
+    )::Cint
+    @test call == :(
+        ccall(:printf, Cint, (Cstring, Cint...), "%d, %d, %d\n", 1, 2, 3)
+    )
+end
+
+@testset "ensure parsecall throws errors appropriately" begin
+    # missing return type
+    @test_throws CcallError parsecall(:( foo(4.0::Cdouble )))
+    # not a function call
+    @test_throws CcallError parsecall(:( foo::Type ))
+    # mismatched types on varargs
+    @test_throws CcallError parsecall(:( foo(x::Cint; y::Cstring, z::Cint)::Cvoid ))
+    # missing type annotations on arguments
+    @test_throws CcallError parsecall(:( foo(x)::Cint ))
+end
+
 
 
 # call some c functions
-@test @ccall(sqrt(4.0::Cdouble)::Cdouble) == 2.0
+@testset "run @ccall with C standard library functions" begin
+    @test @ccall(sqrt(4.0::Cdouble)::Cdouble) == 2.0
 
-const STRING = "hello"
-const BUFFER = Ptr{UInt8}(Libc.malloc((length(STRING) + 1) * sizeof(Cchar)))
-@ccall strcpy(BUFFER::Cstring, STRING::Cstring)::Cstring
-@test unsafe_string(BUFFER) == STRING
+    STRING = "hello"
+    BUFFER = Ptr{UInt8}(Libc.malloc((length(STRING) + 1) * sizeof(Cchar)))
+    @ccall strcpy(BUFFER::Cstring, STRING::Cstring)::Cstring
+    @test unsafe_string(BUFFER) == STRING
 
-# let's write C in Julia. Uppercasing the hard way.
-let buffer = BUFFER
-    while (byte = unsafe_load(buffer)) != 0x00
-        bigger = @ccall toupper(byte::Cint)::Cint
-        unsafe_store!(buffer, bigger) 
-        buffer += 1
+    # let's write C in Julia. Uppercasing the hard way.
+    let buffer = BUFFER
+        while (byte = unsafe_load(buffer)) != 0x00
+            bigger = @ccall toupper(byte::Cint)::Cint
+            unsafe_store!(buffer, bigger)
+            buffer += 1
+        end
     end
-end
 
-@test unsafe_string(BUFFER) == uppercase(STRING)
-Libc.free(BUFFER)
+    @test unsafe_string(BUFFER) == uppercase(STRING)
+    Libc.free(BUFFER)
+end
